8b69939b24379088c610102d62bdd6433b59590a
josaezlopez <josaezlopez@gmail.com>
2024-02-13 12:17:50 +0100

    dependencies fix

--- AsyncMQTT.cpp
+++ AsyncMQTT.cpp
@@ -32,7 +32,6 @@ void AsyncMQTT::setCallback(){
         [this](char* topic, byte* payload, unsigned int length) {
             valueReceived.assign((char*)payload,length);
             topicReceived.assign(topic);
-            //Serial.printf("Recibido %s = %s\r\n",topic,valueReceived.c_str());
             temas[topic].ultimoEstado = valueReceived;
             publishReceived = true;
         }
@@ -100,17 +99,14 @@ void AsyncMQTT::subscribeAll(){
 void AsyncMQTT::subscribe(std::string topic){
 
     if (auto search = temas.find(topic); search != temas.end()){
-       //Serial.printf("Ya está suscrito a %s\r\n",topic);
+       return;
     }
     else{
-        //Serial.printf("Suscribiendo a %s\r\n",topic.c_str());
         tema nuevoTema;
-        //nuevoTema.tema = topic;
         nuevoTema.ultimoEstado = "";
         temas.insert ( std::pair<std::string,tema>(topic,nuevoTema) );
         client->subscribe(topic.c_str());
     }
-
 }
 
 
